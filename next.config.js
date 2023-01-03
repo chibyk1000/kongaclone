73c7b6b58ee0e49cd29e975ff4578a80ae5fd9ba
chibyk1000 <ochibuike798@gmail.com>
2023-01-03 11:10:49 +0100

    solved mongodb  url issues

--- next.config.js
+++ next.config.js
@@ -8,11 +8,11 @@ module.exports = {
   env: {
     BASE_URL: "http://localhost:3000",
     MONGODB_URL:
-      "mongodb+srv://chibyk:Abc123@cluster0.kdq3cxu.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://app124:test123@cluster0.lmdoauu.mongodb.net/?retryWrites=true&w=majority",
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
       "pk_test_51M56IyK4G3r6RCaZDmdtbo5e7RHaQtXFUAipPow57AmACKG0FV9bZP2hF506esX69Lv8FIrQXy0CMY8kOYeghrvx00OmvU3uNd",
   },
-  reactStrictMode: true,  
+  reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com", "www-konga-com-res.cloudinary.com"],
   },
