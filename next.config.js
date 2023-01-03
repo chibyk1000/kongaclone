0de17ce2791645a4ef12bb5a96008cde529b4fda
chibyk1000 <ochibuike798@gmail.com>
2023-01-03 10:46:06 +0100

    more changes

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
   env: {
     BASE_URL: "http://localhost:3000",
     MONGODB_URL:
-      "mongodb+srv://chibyk:<password>@cluster0.kdq3cxu.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://chibyk:Abc123@cluster0.kdq3cxu.mongodb.net/?retryWrites=true&w=majority",
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
       "pk_test_51M56IyK4G3r6RCaZDmdtbo5e7RHaQtXFUAipPow57AmACKG0FV9bZP2hF506esX69Lv8FIrQXy0CMY8kOYeghrvx00OmvU3uNd",
   },
