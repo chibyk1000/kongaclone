7ee26768af4d64200fcff9074cdf46fdfb87c3b4
chibyk1000 <ochibuike798@gmail.com>
2023-01-03 10:03:55 +0100

    made changes to the eslint file

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@
 module.exports = {
   env: {
     BASE_URL: "http://localhost:3000",
-    MONGODB_URL: "mongodb://localhost:27017/KONGA_CLONE",
+    MONGODB_URL:
+      "mongodb+srv://chibyk:<password>@cluster0.kdq3cxu.mongodb.net/?retryWrites=true&w=majority",
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
       "pk_test_51M56IyK4G3r6RCaZDmdtbo5e7RHaQtXFUAipPow57AmACKG0FV9bZP2hF506esX69Lv8FIrQXy0CMY8kOYeghrvx00OmvU3uNd",
   },
